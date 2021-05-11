793c8ef3188cb5828b5562b9e135206d28cf4eb0
Jin-win <baikzeun0520@gmail.com>
2021-05-11 14:10:17 +0900

    컨디션 이미지 1차 변경

--- src/shared/OAuth.js
+++ src/shared/OAuth.js
@@ -1,10 +1,6 @@
-// 클라이언트 설정(테스트)
-// export const REDIRECT_URI = "http://localhost:3000/kakaoLogin";
-// export const CLIENT_ID = "5aad60a70a2fd789ef94b24b1126c2fa";
-
 // 서버 설정(이현님 앱)
 export const CLIENT_ID = "3982623969b121595a2e4fff200e265e";
-//서버 배포용
+//서버 배포용 - 서버를 배포할 땐 이걸 사용합니다
 // export const REDIRECT_URI =
 //   "http://sleepwell.com.s3-website.ap-northeast-2.amazonaws.com/oauth/callback/kakao";
 //로컬 테스트

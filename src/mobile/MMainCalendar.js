6a93544c65b6dfd3d897b50437eac25263862c0b
surfkwon715 <surfkwon715@naver.com>
2021-05-18 17:42:49 +0900

    디테일 수정

--- src/mobile/MMainCalendar.js
+++ src/mobile/MMainCalendar.js
@@ -59,7 +59,7 @@ const AllContainer = styled.div`
  //캘린더 배경
 const CalendarContainer = styled.div`
   width: 100%;
-  height: 55vh;
+  height: 60vh;
   background-color: #F3F3F3;
   margin-top: 1%;
   

e490044a1c3d490a637b4a719f9c1c9bce6fb80b
surfkwon715 <surfkwon715@naver.com>
2021-05-22 23:55:43 +0900

    수정

--- src/components/DetailPost.js
+++ src/components/DetailPost.js
@@ -100,8 +100,8 @@ const DetailPost = (props) => {
             </DateContainer>
             <EmptyTextContainer>
               <EmptyText className="TimeText">
-                {0< hour && hour<6 ? <Text>당신의 시간을 응원합니다!</Text>:null}
-                {6<= hour && hour<11 ? <Text>Sleepwell과 시작하는 하루!</Text>:null}
+                {0< hour && hour<6 ? <Text>당신의 새벽을 응원합니다!</Text>:null}
+                {6<= hour && hour<11 ? <Text>Sleepwell과 시작하는 아침!</Text>:null}
                 {11<= hour && hour <18  ? <Text>Good Afternoon!</Text>:null}
                 {18<= hour && hour <24? <Text>좋은 하루를 보내셨나요?</Text>:null}
                 <Text2>수면기록을 입력해주세요! </Text2>
